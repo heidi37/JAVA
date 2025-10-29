bc877056b19e439dde0d37d844eebaa9dc67fcbc
Heidi Fryzell <heidi37@users.noreply.github.com>
2025-10-29 09:55:42 -0400

    [TASK] Add basic hello programs for homework

---
+++ HelloGoodbye.java
@@ -0,0 +1,6 @@
+
+public class HelloGoodbye {
+  public static void main(String[] args) {
+    System.out.println("Hello " + args[1] + " and " + args[0] + ".");
+  }
+}
--- practice/HelloWorld.java
+++
@@ -1,8 +0,0 @@
-package practice;
-public class HelloWorld {
-   public static void main(String[] args) {
-      // Prints "Hello, World" in the terminal window.
-      System.out.println("Hello, World");
-   }
-}
- 
